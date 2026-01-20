7139937682dd958ad4a51a342ebf467c468b61de
habtesh-beki <habteshbaki2014@gmail.com>
2026-01-20 13:56:11 -0500

    update hospital apis

--- backend/app/api/hospital.py
+++ backend/app/api/hospital.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends, HTTPException , status
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.db.models import Hospital
-from app.schemas.hospital import HospitalCreate, HospitalResponse
+from app.schemas.hospital import HospitalCreate, HospitalResponse, HospitalLogin , TokenResponse
+from app.services.auth_services import authenticate_hospital
 from app.core.security import get_password_hash
-# from backend.app.schemas import hospital
 from sqlalchemy.exc import IntegrityError 
 
 
@@ -14,22 +14,25 @@ router = APIRouter(prefix="/hospitals")
 def create_hospital(hospital: HospitalCreate, db: Session = Depends(get_db)):
     print("Creating hospital:", hospital)
     existing_hospital = db.query(Hospital).filter(
-        Hospital.name == hospital.name,
-        Hospital.address == hospital.address
+        Hospital.hospital_name == hospital.hospital_name,
+        Hospital.registration_number == hospital.registration_number
     ).first()
     print("Existing hospital check:", existing_hospital)
     if existing_hospital:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Hospital '{hospital.name}' already exists at {hospital.address}"
+            detail=f"Hospital '{hospital.hospital_name}' already exists"
         )
     try:
          print("Hashing password for  hospital:", hospital.password)
          password_hash = get_password_hash(hospital.password)
          print("Hashed password:", password_hash)
          new_hospital = Hospital(
-              name=hospital.name,
-              address=hospital.address,
+              registration_number=hospital.registration_number,
+              hospital_name=hospital.hospital_name,
+              admin_name=hospital.admin_name,
+            #   name = hospital.name,
+            #   address= hospital.address,
               phone=hospital.phone,
               email=hospital.email,
               password_hash=password_hash
@@ -68,3 +71,16 @@ def get_hospital(hospital_id: int, db: Session = Depends(get_db)):
 @router.get("/", response_model=list[HospitalResponse])
 def list_hospitals(db: Session = Depends(get_db)):
     return db.query(Hospital).all()
+
+
+@router.post("/login", response_model=TokenResponse)
+def login(data: HospitalLogin, db: Session = Depends(get_db)):
+    print("Login data received:", data)
+    token = authenticate_hospital(db, data.email, data.password)
+    
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
+    return {"access_token": token}
