a90c205ac00a983ee1c802458d4033259e0d70b8
habtesh-beki <habteshbaki2014@gmail.com>
2026-01-20 13:55:03 -0500

    update prediction schema

--- backend/app/schemas/prediction.py
+++ backend/app/schemas/prediction.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , Field
+from typing import Annotated
 
 class PredictionCreate(BaseModel):
     Pregnancies: int
@@ -9,6 +10,17 @@ class PredictionCreate(BaseModel):
     BMI: float
     DiabetesPedigreeFunction: float
     Age: int
+
+class DiabetesPredictionRequest(BaseModel):
+    Pregnancies: Annotated[int, Field(ge=0, le=20)]
+    Glucose: Annotated[float, Field(ge=0, le=300)]
+    BloodPressure: Annotated[float, Field(ge=0, le=200)]
+    SkinThickness: Annotated[float, Field(ge=0, le=100)]
+    Insulin: Annotated[float, Field(ge=0, le=1000)]
+    BMI: Annotated[float, Field(ge=0, le=80)]
+    DiabetesPedigreeFunction: Annotated[float, Field(ge=0, le=5)]
+    Age: Annotated[int, Field(ge=0, le=120)]
+
 
 
 class PredictionResponse(BaseModel):
