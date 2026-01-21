6e7c255dfb89b36f979b0c7732e6480f935d884d
habtesh-beki <habteshbaki2014@gmail.com>
2026-01-21 00:25:21 -0500

    update the model

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, Float, Integer , String, DateTime, ForeignKey, Text
+from sqlalchemy.orm import relationship
 from sqlalchemy.orm import declarative_base
 from datetime import datetime 
 
@@ -8,21 +9,43 @@ Base = declarative_base()
 class Hospital(Base):
     __tablename__ = "hospitals"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), nullable=False)
-    address = Column(Text)  # Added based on HospitalCreate
-    phone = Column(String(20))  # Added based on HospitalCreate
+    registration_number = Column(String(100), unique=True, index=True, nullable=False)
+    hospital_name = Column(String(255), nullable=False)  # More specific name field
+    admin_name = Column(String(255), nullable=False)
+    phone = Column(String(20), nullable=False)
     email = Column(String(255), unique=True, index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    patients = relationship("Patient", back_populates="hospital")
+
 
 class Patient(Base):
     __tablename__ = "patients"
 
-    id = Column(Integer, primary_key=True)
-    hospital_id = Column(Integer, ForeignKey("hospitals.id"))
-    name = Column(String)
-    age = Column(Integer)
-    gender = Column(String)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    id = Column(Integer, primary_key=True, index=True)
+    hospital_id = Column(
+        Integer,
+        ForeignKey("hospitals.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    patient_name = Column(String, nullable=False, index=True)
+    age = Column(Integer, nullable=False)
+    pregnancies = Column(Integer, nullable=False)
+    glucose = Column(Float, nullable=False)
+    blood_pressure = Column(Float, nullable=False)
+    skin_thickness = Column(Float, nullable=False)
+    insulin = Column(Float, nullable=False)
+    bmi = Column(Float, nullable=False)
+    DiabetesPedigreeFunction = Column(Float, nullable=False)
+    risk_probability = Column(Float, nullable=True)
+    risk_level = Column(String, nullable=True)
+    created_at = Column(
+        DateTime(timezone=True),
+        default=datetime.utcnow,
+        nullable=False
+    )
+    hospital = relationship("Hospital", back_populates="patients")
+
 
 class Prediction(Base):
     __tablename__ = "predictions"
