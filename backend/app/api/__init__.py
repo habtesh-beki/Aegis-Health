1682f77d3bec6a35d6965cb9e7524b846878033c
habtesh-beki <habteshbaki2014@gmail.com>
2026-01-21 00:29:31 -0500

    fix the schema and api

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,5 +1,6 @@
 # This makes 'hospital' and 'patient' available when importing from app.api
 from .hospital import router as hospital_router
 from .patient import router as patient_router
+from .prediction import router as prediction_router
 
 __all__ = ["hospital_router", "patient_router"]
