64941d0c5854b30eda3dd70641127e3287a856a6
habtesh-beki <habteshbaki2014@gmail.com>
2026-01-23 14:12:46 -0500

    add update the import file

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,5 +1,7 @@
 from app.db.session import engine
 from app.db.models import Base
+# from db.session import engine
+# from db.models import Base
 
 def init_db():
     Base.metadata.create_all(bind=engine)
