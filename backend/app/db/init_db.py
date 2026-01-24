79f3b431afa8cb55ad4b25e8d6474af93d78c04d
habtesh-beki <habteshbaki2014@gmail.com>
2026-01-24 09:36:34 -0500

    fix the path of prediction

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,7 +1,6 @@
 from app.db.session import engine
 from app.db.models import Base
-# from db.session import engine
-# from db.models import Base
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
